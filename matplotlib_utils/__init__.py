44f675c937b6fe951aa6caf2911a0b950b3bd12d
Dan Davison <dandavison7@gmail.com>
2016-10-09 00:21:20 -0700

    Fix xlim/ylim confusion

--- matplotlib_utils/__init__.py
+++ matplotlib_utils/__init__.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def plot_direction_field(first_derivative_fn, xlim=(-1, 1), ylim=(-1, 1)):
-    Y, X = np.mgrid[xlim[0]:xlim[1]:20j, ylim[0]:ylim[1]:20j]
+    Y, X = np.mgrid[ylim[0]:ylim[1]:20j, xlim[0]:xlim[1]:20j]
     first_deriv = first_derivative_fn(X, Y)
     angle = np.arctan(first_deriv)
     U = np.cos(angle)
